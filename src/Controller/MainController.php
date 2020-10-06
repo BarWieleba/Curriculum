dbbdd6c1b73ca503abfe081bc4978a901e3f9629
Bartłomiej Wieleba <barwieleba@gmail.com>
2020-10-06 22:22:09 +0200

    login through ajax

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,13 +6,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="mainPage")
      */
-    public function mainPage(){
-        return $this->render('index.html.twig');
+    public function mainPage(AuthenticationUtils $authenticationUtils){
+        $token = $this->get('security.token_storage')->getToken()->getUser();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        return $this->render('index.html.twig', array('user'=>$token, 'last_username' => $lastUsername, 'error' => $error));
     }
 }
